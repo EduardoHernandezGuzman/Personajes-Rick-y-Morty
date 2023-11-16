2e5e5de14b0800b843215c2ac9d827509cf9c7f2
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-16 23:36:37 +0000

    Mejora del aspecto visual

--- src/js/component/personajes.jsx
+++ src/js/component/personajes.jsx
@@ -17,10 +17,33 @@ const Personajes = () => {
 
     return (
         <>
-            <h1>Personajes de Rick y Morty</h1>
-            <div>
-            { personajes.map((i)=> <p key={i.id}>{i.name}</p> ) }
+
+            <div className="container my-5" style={{width: "50rem"}}>
+                
+                <div className="position-relative p-5 text-center text-muted fondo border border-dashed rounded-3">
+                    <h1 className="text-body-emphasis">PERSONAJES DE RICK Y MORTY</h1>
+                    <h5 className="col-lg-6 mx-auto" >
+                        Estos 20 personajes han sido traídos desde <span><a className="p" style={{textDecoration:"none"}} href="https://rickandmortyapi.com/">ESTA API</a></span></h5>  
+                        
+                   
+                    <br />
+
+
+
+
+                    {personajes.map((i) => <div className="card tarjetas" style={{ width: "38rem" , height:"24rem" , padding:"10px" , margin:"auto" , marginTop: "18px"}}>
+                        <img src={i.image} style={{width: "13rem" , margin:"auto", marginTop:"9px" }} className="card-img-top" alt={i.name} />
+                        <div className="card-body">
+                            <h2 className="card-text" key={i.id} style={{marginBottom:"16px"}}>{i.name}</h2>
+                            <p className="card-text" key={i.id} style={{margin:"-3px"}}><b>Especie:</b> {i.species}</p>
+                            <p className="card-text" key={i.id} style={{margin:"-3px"}}><b>Género:</b> {i.gender}</p>
+                            <p className="card-text" key={i.id}><b>Lugar de nacimiento:</b> {i.origin.name}</p>
+                        </div>
+                    </div>)}
+                </div>
+
             </div>
+
         </>
     );
 };
